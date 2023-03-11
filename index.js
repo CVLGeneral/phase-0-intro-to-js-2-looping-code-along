cc9a6cb2d6ca3fba98e586db96d100d7e4916337
Leonard Kiprotich <cvlgeneral2020@gmail.com>
2023-03-11 19:32:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+
+const writeCards = (param1,param2) => {
+
+     const messages = []
+
+    for (let i = 0; i < param1.length; i++) {
+
+        const newMessage = `Thank you, ${param1[i]}, for the wonderful ${param2} gift!`;
+       
+        messages.push(newMessage);
+
+
+      }
+
+      return messages;
+}
+
+
+//second assignment 
+
+
+function countDown(){
+
+let countDown = 10;
+
+while (countDown >= 0) {
+console.log(countDown--);
+}
+}
